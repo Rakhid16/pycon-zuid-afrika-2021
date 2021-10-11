1558c42d1257143d5d88bdc6c99c631fb581c00e
Rakhid16 <roploverz@gmail.com>
2021-10-11 13:58:46 +0700

    main function

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import asyncio
 from time import time
 
+from src.try_sqlaclhemy import async_orm_ops
+from src.async_plain_sql import async_db_ops
 from src.sync_plain_sql import sync_db_ops
 from src.db_init import create_new_tables
 
 
+print("Async Plain SQL")
+asyncio.run(create_new_tables())
+for i in range(7):
+    start = time()
+    asyncio.run(async_db_ops())
+    print(f"Insert into two tables took {time() - start} seconds")
+
 print("\nSync Plain SQL")
 asyncio.run(create_new_tables())
-for i in range(12):
+for i in range(7):
     start = time()
     sync_db_ops()
+    print(f"Insert into two tables took {time() - start} seconds")
+
+print("\nAsync SQLAlchemy")
+asyncio.run(create_new_tables())
+for i in range(7):
+    start = time()
+    asyncio.run(async_orm_ops())
     print(f"Insert into two tables took {time() - start} seconds")
